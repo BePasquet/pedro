bfe4c7ec269309be86c7e0d4ae6992749b79efe4
Pedro Benjamin Lescano Pasquet <bepasquet@MacBook-Pro-Pedro.local>
2020-08-31 11:00:13 +0700

    - products container comments removed

--- libs/products/src/lib/containers/products/products.container.ts
+++ libs/products/src/lib/containers/products/products.container.ts
@@ -75,31 +75,12 @@ export class Products {
     private readonly store: Store<ProductsPartialState>,
     private readonly changeDetectorRef: ChangeDetectorRef
   ) {
-    /*
-      Hi Ben i write some comments to walk you through so is not so tedious to read
-      hope you don't grab your head when you see the code haha
-    */
-
-    /* 
-      Global state the selector is on top of the component this will be used data 
-      from store on the component in this case is products state but can be a composition of product, user, etc
-    */
     const storeSlice$ = this.store.pipe(select(selectProductComponentState));
 
-    /* 
-      Represents state that is contain in the component and can change state can be updated via setSate ex: this.setState.next({page: 1, ...etc});
-    */
-    const componentStateSlice$ = this.setState.asObservable();
-
-    /*
-      Method to merge component state with global state from store, when components get to big i don't know if is it better
-      to use a reducer actions and effects for component state.
-      also trigger change detection on observable next this is use for zoneless app (ecommerce mobile ngZone is set noop)
-    */
     const state$ = stateChanges(
       PRODUCTS_COMPONENT_INITIAL_STATE,
       storeSlice$,
-      componentStateSlice$
+      this.setState
     ).pipe(
       tap((state) => {
         this.state = state;
@@ -109,27 +90,13 @@ export class Products {
 
     this.subscriptions.add(state$.subscribe());
 
-    /*
-      The code bellow works in the same way instead of keep in state of component keep state of filter
-      i started using this pattern with merge and scan, before i was using combineLatest as 
+    const categorySlice$ = this.category$.pipe(
+      map((category) => ({ category }))
+    );
 
-      ``
-          combineLatest([this.category$, this.orderBy$, this.query$])
-          .pipe(
-            map(([category, sort, name]) => ({ category, sort, name }))
-          );
-      ``
-      i don't know what will be better
-    */
-    const categorySlice$ = this.category$
-      .asObservable()
-      .pipe(map((category) => ({ category })));
+    const orderBySlice$ = this.orderBy$.pipe(map((orderBy) => ({ orderBy })));
 
-    const orderBySlice$ = this.orderBy$
-      .asObservable()
-      .pipe(map((orderBy) => ({ orderBy })));
-
-    const nameSlice$ = this.query$.asObservable().pipe(
+    const nameSlice$ = this.query$.pipe(
       debounceTime(250),
       distinctUntilChanged(),
       map((name) => ({ name }))
@@ -142,7 +109,7 @@ export class Products {
       nameSlice$
     ).pipe(startWith(CONTROLS_INITIAL_STATE), map(controlsToProductFilter));
 
-    const requestMoreProducts$ = this.pagination$.asObservable().pipe(
+    const requestMoreProducts$ = this.pagination$.pipe(
       withLatestFrom(filterChanges$),
       tap(([pagination, filter]) => {
         this.setState.next({
@@ -162,11 +129,6 @@ export class Products {
       })
     );
 
-    /* 
-      this is just not to do 
-      this.subscriptions.add(requestProducts$$.subscribe());
-      this.subscriptions.add(requestMoreProducts$.subscribe());
-    */
     const effects$ = merge(requestProducts$, requestMoreProducts$);
 
     this.subscriptions.add(effects$.subscribe());
