a8f401e041fd0fc239b5f8d41b265d48ea99a29b
Pedro Benjamin Lescano Pasquet <bepasquet@MacBook-Pro-Pedro.local>
2020-12-09 17:43:49 +0700

    - let module added

--- apps/ecomerce-mobile/src/app/app.module.ts
+++ apps/ecomerce-mobile/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { ENVIRONMENT_TOKEN_KEY } from '@pedro/core';
+import { LetModule } from '@rx-angular/template';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -21,6 +21,7 @@ import { AppComponent } from './app.component';
       logOnly: environment.production,
     }),
     HttpClientModule,
+    LetModule,
   ],
   providers: [{ provide: ENVIRONMENT_TOKEN_KEY, useValue: environment }],
   bootstrap: [AppComponent],
